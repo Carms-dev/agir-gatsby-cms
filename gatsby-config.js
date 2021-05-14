64d05db64bbcb9ff1c2cba61be81329fe3c17309
Carms Ng <carmenhyng89@gmail.com>
2021-05-14 00:13:16 -0400

    [WIP] Set up gatsby-source-wordpress

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
         url:
         // allows a fallback url if WPGRAPHQL_URL is not set in the env, this may be a local or remote WP instance.
           process.env.WPGRAPHQL_URL ||
-          `http://localhost:10003/graphql`,
+          `https://www.agirtest.club/graphql`,
         schema: {
           //Prefixes all WP Types with "Wp" so "Post and allPost" become "WpPost and allWpPost".
           typePrefix: `Wp`,
@@ -50,15 +50,10 @@ module.exports = {
           hardCacheMediaFiles: true,
         },
         type: {
-          Post: {
-            limit:
-              process.env.NODE_ENV === `development`
-                ? // Lets just pull 50 posts in development to make it easy on ourselves (aka. faster).
-                  50
-                : // and we don't actually need more than 5000 in production for this particular site
-                  5000,
-          },
-        },
+          __all: {
+              limit: process.env.NODE_ENV === `development` ? 50 : null
+          }
+        }
       },
     },
   ],
--- gatsby-node.js
+++ gatsby-node.js
@@ -7,5 +7,37 @@
 // You can delete this file if you're not using it
 
 
-// TODO: Use WordPress dataimplementing the createPages API in gatsby-node.js
-// https://github.com/gatsbyjs/gatsby-starter-wordpress-blog/blob/main/gatsby-node.js
+// TODO: Implement the createPages API in gatsby-node.js
+// demo: https://github.com/gatsbyjs/gatsby-starter-wordpress-blog/blob/main/gatsby-node.js
+
+const path = require(`path`)
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return graphql(`
+    {
+      allWpPost(sort: { fields: [date] }) {
+        nodes {
+          title
+          excerpt
+          content
+          slug
+        }
+      }
+    }
+  `).then(result => {
+    //highlight-start
+    result.data.allWpPost.nodes.forEach(node => {
+      createPage({
+        path: node.slug,
+        component: path.resolve(`./src/templates/blog-post.js`),
+        context: {
+          // This is the $slug variable
+          // passed to blog-post.js
+          slug: node.slug,
+        },
+      })
+    })
+    //highlight-end
+  })
+}
