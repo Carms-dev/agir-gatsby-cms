80530b69ad05be2c75f27df1a9d5cbc7e03ab276
Carms Ng <carmenhyng89@gmail.com>
2021-05-25 22:29:21 -0400

    [ADD] Logo component

---
+++ src/components/logo.js
@@ -0,0 +1,34 @@
+import * as React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
+
+export default function Logo() {
+  const { wp: { siteLogo } } = useStaticQuery(
+    graphql`
+      query {
+        wp {
+          siteLogo {
+            altText
+            localFile {
+              childImageSharp {
+                gatsbyImageData(
+                  width: 100,
+                  placeholder: BLURRED
+                  layout: CONSTRAINED
+                )
+              }
+            }
+          }
+        }
+      }        
+    `
+  );
+
+  return (
+    <>
+      <GatsbyImage
+        image={siteLogo.localFile.childImageSharp.gatsbyImageData}
+        alt={siteLogo.altText} />
+    </>
+  )
+}
